a7d503b54f0bd85979df4969b2909e24983abf94
willie capre <wcap18@gmail.com>
2019-08-07 17:14:42 -0400

    added file
    starting project

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+
+/* Treehouse FSJS Techdegree
+ * Project 5 - Public API Requests
+ This project is to get random employee information in the directorty.
+ * app.js */
+
+console.log('inscript');
+// var request = new HMLHttRequest();
+// request.data = function () {
+    
+// };
+// request.open('GET', 'https://randomuser.me/api/');
+// request.send();
+const gallery = $('#gallery');
+for (var i=0; i< 12; i+=1){
+   gallery.append(i);
+
+    $.ajax({
+        url: 'https://randomuser.me/api/',
+        dataType: 'json',
+        success: function(data) {
+          console.log(data);
+          FileList = data.result;
+        }
+      });
+}
+
+
+document.getElementById('#gallery');
