5cf9c53021c620b4b393958c2781f83355123bf0
willie capre <wcap18@gmail.com>
2019-08-07 17:47:51 -0400

    created function
    for (statement 1; statement 2; statement 3) {
      // code block to be executed
    }

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,33 @@ console.log('inscript');
 // fetching data from url.
 fetch('https://randomuser.me/api/')
   .then(response => response.json()) 
-  .then(data => getUsers(data.result)); 
+  .then(data => fetchUsers(data.result)); 
   
+// create 12 users
+function fetchUsers(data) {
+  const galleryOfUsers = ""
+  var i;
+  for (i = 0; i < data.length; i++) {
+    
 
+    }
+ 
+ 
+ 
+ 
+ 
+ 
+ <div class="card">
+                    <div class="card-img-container">
+                        <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+                    </div>
+                    <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">first last</h3>
+                        <p class="card-text">email</p>
+                        <p class="card-text cap">city, state</p>
+                    </div>
+                </div>
+}
 
 
 // var request = new HMLHttRequest();
