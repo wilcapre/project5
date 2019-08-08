f47af714b629b211bfaf310ce72ad1333babd611
willie capre <wcap18@gmail.com>
2019-08-08 18:08:21 -0400

    added directory
    directory not working

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,17 @@
  This project is to get random employee information in the directorty.
  * app.js */
 
-console.log('inscript');
+//console.log('inscript');
 
 // fetching data from url.
-fetch('https://randomuser.me/api/')
+fetch('https://randomuser.me/api?results=12&nat=us')
   .then(response => response.json()) 
-  .then(data => fetchUsers(data.result)); 
+  .then(data => fetchUsers(data.results)); 
   
 // create 12 users
 function fetchUsers(data) {
-  const galleryOfUsers = ""
-  var i;
-  for (i = 0; i < data.length; i++) {
+  let galleryOfUsers = ""
+  for (let i = 0; i < data.length; i++) {
     galleryOfUsers += 
 `
  <div class="card">
@@ -29,30 +28,65 @@ function fetchUsers(data) {
      </div>
  </div>
  `;
-document.querySelector('#gallery').innerHTML = galleryOfUsers;
+  document.querySelector('#gallery').innerHTML = galleryOfUsers;
 
+  $('.card').on('click', function(){
+    let thisIndex = $('.card').index(this);
+    modalMarkup(data[thisIndex]);
+  });
+  } 
 }
 
+fetch('https://randomuser.me/api?inc=image,name,location,email,cell,birthday,address')
+  .then(response => response.json()) 
+  .then(data => fetchDirectory(data.results)); 
 
+function fetchDirectory(data) {
+  let modalCloseBtn = ""
+  for (let i = 0; i < data.length; i++) {
+     modalCloseBtn  += 
+`
+<div class="modal-container">
+<div class="modal">
+    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+    <div class="modal-info-container">
+        <img class="modal-img" src="${data[i].picture}" alt="profile picture">
+        <h3 id="name" class="modal-name cap">${data[i].name}</h3>
+        <p class="modal-text">${data[i].email}</p>
+        <p class="modal-text cap">${data[i].city}</p>
+        <hr>
+        <p class="modal-text">${data[i].cell}(555)555-5555</p>
+        <p class="modal-text">${data[i].address}123 Portland Ave., Portland, OR 97204</p>
+        <p class="modal-text">${data[i].Birthday} 10/21/2015</p>
+    </div>
+</div>
+
+ `; 
+  document.querySelector('#modalCloseBtn').innerHTML = modalCloseBtn;
+
+  $('.modal').on('click', function(){
+    let thisIndex = $('.modal').index(this);
+    modalMarkup(data[thisIndex]);
+  });
+  }
+
+}
 // var request = new HMLHttRequest();
 // request.data = function () {
     
 // };
 // request.open('GET', 'https://randomuser.me/api/');
 // request.send();
-const gallery = $('#gallery');
-for (var i=0; i< 12; i+=1){
-   gallery.append(i);
+// const gallery = $('#gallery');
+// for (var i=0; i< 12; i+=1){
+//    gallery.append(i);
 
-    $.ajax({
-        url: 'https://randomuser.me/api/',
-        dataType: 'json',
-        success: function(data) {
-          console.log(data);
-          FileList = data.result;
-        }
-      });
-}
-
-
-document.getElementById('#gallery');
+//     $.ajax({
+//         url: 'https://randomuser.me/api/',
+//         dataType: 'json',
+//         success: function(data) {
+//           console.log(data);
+//           FileList = data.result;
+//         }
+//       });
+// 
