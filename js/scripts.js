e93257169b1cd52e24ba104b15bec48e9e8263fb
willie capre <wcap18@gmail.com>
2019-08-15 14:52:23 -0400

    added append
    append is  not doing anthing when click on the user

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,10 @@ modalList.style.display = 'none';
 const script = document.getElementsByTagName('script')
 document.body.insertBefore(modalList, script[0])
 //append div to modalList 
-$('button').click(function() {
-  $('modalList').append('div');
-});
+//const modal-close-btn = document.querySelector('div')
+// $('button').click(function() {
+//   $('modalList').append('div');
+// });
 
 // fetching data from url.
 fetch('https://randomuser.me/api?results=12&nat=us')
@@ -23,33 +24,73 @@ fetch('https://randomuser.me/api?results=12&nat=us')
   
 // create 12 users
 function fetchUsers(data) {
-  let galleryOfUsers = ""
+  let galleryOfUsers = []
   for (let i = 0; i < data.length; i++) {
-    galleryOfUsers += 
     
+    const users = ($(`
+    <div class="card">
+        <div class="card-img-container">
+          <img class="card-img" src="${data[i].picture.large}" alt="profile picture">
+         </div>
+         <div class="card-info-container">
+          <h3 id="name" class="card-name cap">${data[i].name.first} ${data[i].name.last}</h3>
+          <p class="card-text">${data[i].email}</p>
+          <p class="card-text cap">${data[i].location.city}</p>
+        </div>
+    </div>`));
+    galleryOfUsers.push(users);
+    const userModal = $(`<div class="modal-container" style="display:none">
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+            <h3 id="name" class="modal-name cap">name</h3>
+            <p class="modal-text">email</p>
+            <p class="modal-text cap">city</p>
+            <hr>
+            <p class="modal-text">(555) 555-5555</p>
+            <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+            <p class="modal-text">Birthday: 10/21/2015</p>
+        </div>
+    </div>
+</div>`);
+// userModal.style.display = 'none'
+    users.append(userModal);
+  $(users).on('click', `#${data[i].login.uuid}`,function (){
+ console.log('users');
+    });
 //Html for the gallery modul of the 12 users
-`
- <div class="card">
-     <div class="card-img-container">
-       <img class="card-img" src="${data[i].picture.large}" alt="profile picture">
-      </div>
-      <div class="card-info-container">
-       <h3 id="name" class="card-name cap">${data[i].name.first} ${data[i].name.last}</h3>
-       <p class="card-text">${data[i].email}</p>
-       <p class="card-text cap">${data[i].location.city}</p>
-     </div>
- </div>
- `;
- let modalMarkup;
-  document.querySelector('#gallery').innerHTML = galleryOfUsers;
-  $('.card').on('click',function(){
-    let currentIndex = $('.card').index(this);
-   // modalMarkup(data[currentIndex]);
-    //console.log(typeof data[currentIndex]);
-    
+
+  $('#gallery').append(galleryOfUsers);
+  
+  //let modalMarkup;
+  
+  $('.card').on('click',function(event){
+    //$('.card').show();
+//     let div = `<div class="modal-container">
+//     <div class="modal">
+//         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+//         <div class="modal-info-container">
+//             <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+//             <h3 id="name" class="modal-name cap">name</h3>
+//             <p class="modal-text">email</p>
+//             <p class="modal-text cap">city</p>
+//             <hr>
+//             <p class="modal-text">(555) 555-5555</p>
+//             <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+//             <p class="modal-text">Birthday: 10/21/2015</p>
+//         </div>
+//     </div>
+// </div>`;
+
+    // let currentIndex = $('.card').index(this);
+    // $(event.target).append(div);
+    // card.style.display = 'none';
+    // console.log(typeof data[currentIndex]);
+    // //modalMarkup(data[currentIndex]);
+
   });
   } 
-
 }
 
 // fetch('https://randomuser.me/api?inc=image,name,location,email,cell,birthday,address')
@@ -66,8 +107,7 @@ function fetchDirectory(info) {
   let day = dob.substr(8, 2);
   let year= dob.substr(2, 2);
 
-
-  //let window = ""
+//let window = ""
   //window += `<div class=“modal-container”>`
 
 //Html to show directory of users
@@ -90,12 +130,16 @@ function fetchDirectory(info) {
 
   //document.querySelector('div').innerHTML += window;
 
-
   $('.modal').on('click', function(){
+    $('.modal').remove();
     let thisIndex = $('.modal').index(this);
     modalMarkup(data[thisIndex]);
   });
   }
+// close window
+$('#modal-close-btn').on('click', function (){
+   $('.modal-container').remove();
+  })
 
 
 // var request = new HMLHttRequest();
